07d48ef730b80953148eea21dc8a5c6baeb0e8da
unknown <miruyewesley@gmail.com>
2024-04-11 00:50:17 -0700

    sixth commit

---
+++ week_5/web_design/_/main.py
@@ -0,0 +1,18 @@
+import flet as ft
+
+def maiin(page: ft.page):
+    page. title= "learning FLET"
+    content = ft.column(
+        spacing =20,
+        control=[
+    
+
+    ft.Text ("this  is a simple flet application"size 32 , weight="w600"),
+    ft.Text("welcome to learning flet")
+        ]
+        ft.Row(spacing = 20,
+               ft.text("this is a row in flet"))
+
+    )ys
+    #DISPLAY OUT SCREEN
+    page.add()
